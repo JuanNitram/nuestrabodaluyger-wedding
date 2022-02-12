772541ef1304ef2d57985f145edf0e65ef7c67ac
Juan Vargas <juanmvg2006@gmail.com>
2022-02-11 22:03:54 -0300

    Update cards

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -93,7 +93,7 @@
                         </div>
                     </div>
                     <div class="row bord-box bg-img" style="display: flex; justify-content: center" data-background="images/slider.jpg">
-                        <div class="col-md-3 item-box" style="height: 520px; margin-right: 10px; margin-left: 10px">
+                        <div class="col-md-3 item-box" style="height: 520px;">
                             <h2 class="custom-font numb">01</h2>
                             <h6 class="title">Ceremonia</h6>
                             <p>
@@ -104,12 +104,18 @@
                                 Por protocolo COVID se solicita confirmar presencia hasta el 5 de Marzo y se debe adjuntar certificado de vacunación.
                             </p>
                         </div>
-                        <div class="col-md-3 item-box" style="height: 520px; margin-right: 10px; margin-left: 10px">
+                        
+                        <div class="col-md-3 item-box" style="height: 520px;">
                             <h2 class="custom-font numb">02</h2>
+                            <h6 class="title">Vestimenta</h6>
+                            <p>La vestimenta de la ceremonia es formal.</p>
+                        </div>
+                        <div class="col-md-3 item-box" style="height: 520px;">
+                            <h2 class="custom-font numb">03</h2>
                             <h6 class="title">Colectivo</h6>
                             <p>
                                 Mariana Castillo: <br/>
-                                BROU PESOS 001504761 - 00002
+                                BROU PESOS - CAJA AHORRO - 001504761 - 00002
                             </p>
                             <p>
                             </p>
@@ -117,13 +123,13 @@
                             </p>
                             <p>
                                 Schubert Reyes:<br/>
-                                BROU DOLARES 001349053 - 00004
+                                BROU DOLARES - CAJA AHORRO - 001349053 - 00004
                             </p>
                         </div>
-                        <div class="col-md-3 item-box" style="height: 520px; margin-right: 10px; margin-left: 10px">
-                            <h2 class="custom-font numb">03</h2>
-                            <h6 class="title">Vestimenta</h6>
-                            <p>La vestimenta de la ceremonia es formal.</p>
+                        <div class="col-md-3 item-box" style="height: 520px;">
+                            <h2 class="custom-font numb">04</h2>
+                            <h6 class="title">Civil</h6>
+                            <p>Viernes 11 de Marzo 10:00, Intendencia de Rivera. </p>
                         </div>
                     </div>
                 </div>
